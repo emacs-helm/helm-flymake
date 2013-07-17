1ee82190d00948ad9e504180e016a4e1d8628f32
Akira Tamamori <tamamori5917@gmail.com>
2013-07-17 13:04:52 +0900

    Update comments about prefix argument.

--- helm-flymake.el
+++ helm-flymake.el
@@ -25,6 +25,8 @@
 ;; `helm' interface for flymake.
 ;; When `flymake-mode' is t, M-x `helm-flymake' lists warning and error
 ;; messages in *helm flymake* buffer.
+;; C-u M-x `helm-flymake' insert the line number of current cursor position
+;; into minibuffer.
 ;;
 
 ;;; Installation:
